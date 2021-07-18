04c5cac4c393f42963f0532457c7892d1dcdf259
Lloyd B. Park <lloyd.b.park@gmail.com>
2021-07-18 10:14:11 -0400

    Fixed string_to_unix_time.  My code was inadvertantly using a int64 when it needed to use a word64. But FreePascal now includes conversion routines which work.  So I greatly simplified the program.

--- apps/string-to-unix-time/string_to_unix_time.pas
+++ apps/string-to-unix-time/string_to_unix_time.pas
@@ -4,8 +4,8 @@ program string_to_unix_time;
 
 uses
    lbp_current_time,
-   baseunix,
-   unixtype,
+   lbp_types,
+   dateutils,
    sysutils;
 
 const
@@ -32,61 +32,14 @@ procedure Usage();
    
    
 // ************************************************************************
-// * GetMS1970() - Returns the timezone adjustment in MS
-// ************************************************************************
-
-function ms1970(): comp;
-   var
-      dt1970:    tDateTime;
-      ts1970:    tTimeStamp;
-   begin
-      dt1970:= StrToDateTime( '01-01-70 00:00:00');
-      ts1970:= DateTimeToTimeStamp( dt1970);
-      result:= TimeStampToMSecs( ts1970) - (GMTOffset * 3600000);
-   end; // ms1970()
-
-
-// ************************************************************************
-// * DateTimeToUnixTime() - Convert the pascal DateTime variable to a 
-// ************************************************************************
-
-function DateTimeToUnixTime( const DateTime: tDateTime): time_t;
-   var
-      TimeStamp: tTimeStamp;
-      ms:        comp;
-            
-   begin
-      TimeStamp:= DateTimeToTimeStamp( DateTime);
-      ms:= TimeStampToMSecs( TimeStamp);
-      result:= trunc((ms - ms1970) / 1000);
-   end; // DateTimeToUnixTime()
-   
-
-// ************************************************************************
-// * UnixTimeToDateTime() - Convert UNIX seconds since 1970-01-01 to a
-// *                        pascal tDateTime variable.
-// ************************************************************************
-
-function UnixTimeToDateTime( UnixTime: time_t): tDateTime;
-   var
-      TimeStamp: tTimeStamp;
-      ms:        comp;
-   begin
-      ms:= (comp( UnixTime) * comp(1000)) + ms1970;
-      TimeStamp:= MSecsToTimeStamp( ms);
-      result:= TimeStampToDateTime( TimeStamp);
-   end; // UnixTimeToDateTime()            
-
-
-// ************************************************************************
 // * main() 
 // ************************************************************************
 
 var
    P:         string;  // First parameter on the command line.
-   UnixTime:  time_t;
+   UnixTime:  word64;
    Code:      word;    // Conversion errors
-   lbpDT:    tlbpTimeClass;   
+   lbpDT:     tlbpTimeClass;   
 begin
    if( ParamCount <> 1) then begin
       Usage(); // Aborts the program
@@ -96,13 +49,13 @@ begin
    P:= ParamStr( 1);
    if( Pos( ':', P) > 0) then begin
       lbpDT.Str:= P;
-      writeln(  DateTimetoUnixTime( lbpDT.TimeOfDay));
+      writeln(  DateTimetoUnix( lbpDT.TimeOfDay));
    end else begin
       Val( P, UnixTime, Code); // Convert the string representation of UnixTime to a time_t;
       if( Code > 0) then begin
          Usage();  // Aborts the program
       end;
-      lbpDT.TimeOfDay:= UnixTimeToDateTime( UnixTime);
+      lbpDT.TimeOfDay:= UnixToDateTime( UnixTime);
       writeln( lbpDT.Str); 
    end;
    lbpDT.Destroy();
